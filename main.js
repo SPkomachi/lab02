06019500d38e0e343c3b2a6255ad39e1b443dcc3
SPkomachi <lookyeemoo@gmail.com>
2021-07-09 10:25:45 +0700

    3.5

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const app = Vue.createApp({
             brand: 'SE 331',
             // image: './assets/images/socks_green.jpg',
             inStock: false,
-            inventory: 100,
+            onSale: false,
+            inventory: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',
@@ -21,18 +22,32 @@ const app = Vue.createApp({
         title(){
             return this.brand + '' + this.product
         },
-        image()
-        {
-        return this.variants[this.selectedVariant].image;
+        image(){
+        return this.variants[this.selectedVariant].image
         },
-        inStock()
-        {
-        return this.variants[this.selectedVariant].quantity;
+        inStock(){
+        return this.variants[this.selectedVariant].quantity
+        }, 
+        saleMessage() {
+        if (this.onSale==true) {
+            return this.brand + ' ' + this.product + ' is on sale.'
         }
-    },
+        else{
+
+        }
+        return ''
+    }
+},
     methods: {
         addToCart() {
             this.cart += 1
+            this.inventory -= 1
+                if (this.inventory < 0){
+                    this.inventory = 0
+                    this.cart = 0
+                    // this.inStock = true
+                    // this.onSale = true
+                }
         },
         updateImage(variantImage) {
             this.image = variantImage
@@ -40,5 +55,6 @@ const app = Vue.createApp({
         updateVariant(index) {
             this.selectedVariant = index;
         }
-    }
+    },
+   
 })
