90b3d943d209178c07ce00fdf469cb4f1800f61f
SPkomachi <lookyeemoo@gmail.com>
2021-07-09 10:34:23 +0700

    4.3

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,7 +1,7 @@
 app.component('product-display', {
     template: 
       /*html*/ 
-      `<div class="product-display">
+    `<div class="product-display">
         <div class="product-container">
           <div class="product-image">
             <img v-bind:src="image">
@@ -29,5 +29,36 @@ app.component('product-display', {
             </button>
           </div>
         </div>
-      </div>`
-  })
+      </div>`,
+      data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ]
+        }
+      },
+      methods: {
+          addToCart() {
+              this.cart += 1
+          },
+          updateVariant(index) {
+              this.selectedVariant = index
+          }
+      },
+      computed: {
+          title() {
+              return this.brand + ' ' + this.product
+          },
+          image() {
+              return this.variants[this.selectedVariant].image
+          },
+          inStock() {
+              return this.variants[this.selectedVariant].quantity
+          }
+      }
+})
